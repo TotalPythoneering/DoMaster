2c800bd13a2a46ebb60b22d63e999e63f13236e1
Randall Nagy <randall.nagy@gmail.com>
2026-01-29 12:30:21 -0500

    .

--- domaster/test_cases.py
+++
@@ -1,8 +0,0 @@
-# MISSION: Manage a to-do list or / and ideas.
-# STATUS: Obsolete
-# VERSION: 0.1.1
-# NOTES: Lighty tested. See the project for full documentation.
-# DATE: 2026-01-28 08:50:11
-# FILE: test_cases.py
-# AUTHOR: Randall Nagy
-#
--- domaster/test_sync.py
+++
@@ -1,8 +0,0 @@
-# MISSION: Manage a to-do list or / and ideas.
-# STATUS: Obsolete
-# VERSION: 1.1.1
-# NOTES: Lighty tested. See the project for full documentation.
-# DATE: 2026-01-28 08:50:51
-# FILE: test_sync.py
-# AUTHOR: Randall Nagy
-#
